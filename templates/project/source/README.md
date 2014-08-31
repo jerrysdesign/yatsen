9ebe5f3cc03236f8af2b936689a743e76c515e7b
jerrysdesign <jerry7722@hotmail.com>
2014-08-31 19:54:12 +0800

    Start!

---
+++ templates/project/source/README.md
@@ -0,0 +1,11 @@
+Sass + SMACSS + OOCSS
+==============
+
+Start project with Sass. To know more, go to : http://smacss.com
+
+Based on
+==
+ - Idiomatic css, https://github.com/necolas/idiomatic-css/tree/master/translations/zh-TW
+ - Boilerplate, https://github.com/h5bp/html5-boilerplate
+ - Normalize.css, https://github.com/necolas/normalize.css/
+ - Compass, http://compass-style.org/
---
+++ templates/project/source/config.rb
@@ -0,0 +1,42 @@
+# Require any additional compass plugins here.
+require 'slim'
+Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :format => :html5
+
+# Set this to the root of your project when deployed:
+http_path = "../"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
+
+css_dir = "css" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "img" # by Fire.app 
+javascripts_dir = "js" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :compact # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>false} # by Fire.app 
