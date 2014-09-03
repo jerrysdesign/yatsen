317a5a0f9f1f9bed1441a3bb74e1158e5de0ad9b
jerrysdesign <jerry7722@hotmail.com>
2014-09-04 02:15:02 +0800

    改用 jquerytools

--- templates/project/source/js/script.js
+++ templates/project/source/js/script.js
@@ -1,40 +1,46 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	$('.search-tab ul').find('> li:eq(0)').addClass('active');
+// 	$('.search-tab ul').find('> li:eq(0)').addClass('active');
 	
-	$('.search-tab ul li a').click(function () {
-		var tab = $(this).closest('.search-utility'),
-			index = $(this).closest('li').index();
+// 	$('.search-tab ul li a').click(function () {
+// 		var tab = $(this).closest('.search-utility'),
+// 			index = $(this).closest('li').index();
 		
-		tab.find('ul > li').removeClass('active');
-		$(this).closest('li').addClass('active');
+// 		tab.find('ul > li').removeClass('active');
+// 		$(this).closest('li').addClass('active');
 		
-		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').slideUp(300);
-		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').slideDown(300);
+// 		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').slideUp(300);
+// 		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').slideDown(300);
 		
-	});
-});
+// 	});
+// });
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	$('.hot__tab').find('> li:eq(0)').addClass('active');
+// 	$('.hot__tab').find('> li:eq(0)').addClass('active');
 	
-	$('.hot__tab li a').click(function () {
-		var tab = $(this).closest('.hot'),
-			index = $(this).closest('.hot__tab > li').index();
+// 	$('.hot__tab li a').click(function () {
+// 		var tab = $(this).closest('.hot'),
+// 			index = $(this).closest('.hot__tab > li').index();
 		
-		tab.find('.hot__tab > li').removeClass('active');
-		$(this).closest('li').addClass('active');
+// 		tab.find('.hot__tab > li').removeClass('active');
+// 		$(this).closest('li').addClass('active');
 		
-		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').hide();
-		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').show();
+// 		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').hide();
+// 		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').show();
 		
-	});
-});
+// 	});
+// });
 
 $(function() {
+
+	$(".search-tab > ul").tabs(".tabs_item");
+	$(".hot__tab").tabs(".tabs_item");
+
 	$(".vertical").scrollable({
 		vertical:true,
-		size: 1
-	}).mousewheel();
+		size: 1,
+		mousewheel: true
+	});
+	
 });
