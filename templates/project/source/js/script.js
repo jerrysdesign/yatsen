a6a7b39e1f4aa8e80a500fe611cd654f69dfbc59
jerrysdesign <jerry7722@hotmail.com>
2014-09-07 15:02:48 +0800

    整理

--- templates/project/source/js/script.js
+++ templates/project/source/js/script.js
@@ -1,41 +1,7 @@
-// $(document).ready(function() {
-
-// 	$('.search-tab ul').find('> li:eq(0)').addClass('active');
-	
-// 	$('.search-tab ul li a').click(function () {
-// 		var tab = $(this).closest('.search-utility'),
-// 			index = $(this).closest('li').index();
-		
-// 		tab.find('ul > li').removeClass('active');
-// 		$(this).closest('li').addClass('active');
-		
-// 		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').slideUp(300);
-// 		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').slideDown(300);
-		
-// 	});
-// });
-
-// $(document).ready(function() {
-
-// 	$('.hot__tab').find('> li:eq(0)').addClass('active');
-	
-// 	$('.hot__tab li a').click(function () {
-// 		var tab = $(this).closest('.hot'),
-// 			index = $(this).closest('.hot__tab > li').index();
-		
-// 		tab.find('.hot__tab > li').removeClass('active');
-// 		$(this).closest('li').addClass('active');
-		
-// 		tab.find('.tab-content').find('.tabs_item').not('.tabs_item:eq(' + index + ')').hide();
-// 		tab.find('.tab-content').find('.tabs_item:eq(' + index + ')').show();
-		
-// 	});
-// });
-
 $(function() {
 
-	$(".search-tab > ul").tabs(".search-utility .tabs_item");
-	$(".hot__tab").tabs(".hot .tabs_item");
+	$(".search-tab > ul").tabs(".search-utility .tabs-item");
+	$(".hot__tab").tabs(".hot .tabs-item");
 
 	$(".vertical").scrollable({
 		vertical:true,
@@ -53,7 +19,3 @@ $(function() {
 		});
 	};
 })(jQuery);
-// $(function(){
-// 	var _def = $('.hot__list li:odd');
-// 	$(this).striped(_def);
-// });
