982fe7ecdcbc556f939d10bd60205821da437415
jerrysdesign <jerry7722@hotmail.com>
2014-09-20 18:50:23 +0800

    bulid 2014-09-20-18-50-06

---
+++ build/js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$(".search-tab > ul").tabs(".search-utility .tabs-item");
+	$(".hot__tab").tabs(".hot .tabs-item");
+	$(".function-tab").tabs(".function-utility .tabs-item");
+	$(".search__tab").tabs(".searchs .tabs-item");
+
+	$(".vertical").scrollable({
+		vertical:true,
+		size: 1,
+		mousewheel: true
+	});
+	
+});
+
+;(function ($){
+	$.fn.striped = function (tag){
+		var $this = $(this);
+		$this.each(function(){
+			tag.addClass("odd");
+		});
+	};
+})(jQuery);
