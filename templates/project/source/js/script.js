2be471333ea7ade0172146bf8ed8cea264427079
jerrysdesign <jerry7722@hotmail.com>
2014-11-27 07:10:41 +0800

    Specifies the tab2 that is initially opened when the page loads
    ==>initialIndex: 1

--- templates/project/source/js/script.js
+++ templates/project/source/js/script.js
@@ -3,8 +3,9 @@ $(function() {
 	$(".search-tab > ul").tabs(".search-utility .tabs-item");
 	$(".hot__tab").tabs(".hot .tabs-item");
 	$(".function-tab").tabs(".function-utility .tabs-item");
-	$(".search__tab").tabs(".searchs .tabs-item");
-
+	$(".search__tab").tabs(".searchs .tabs-item",{
+		initialIndex: 1
+	});
 	$(".vertical").scrollable({
 		vertical:true,
 		size: 1,
